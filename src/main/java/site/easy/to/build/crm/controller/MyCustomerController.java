44b205dc2ccafe8f3534c12af8fe915a53cbea71
randriamanantenavony <152492886+randriamanantenavony@users.noreply.github.com>
2025-03-24 23:30:15 +0300

    Update MyCustomerController
    Update MyCustomerController

--- src/main/java/site/easy/to/build/crm/controller/MyCustomerController.java
+++ src/main/java/site/easy/to/build/crm/controller/MyCustomerController.java
@@ -9,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import site.easy.to.build.crm.entity.Customer;
+import site.easy.to.build.crm.entity.Lead;
+import site.easy.to.build.crm.entity.Ticket;
 import site.easy.to.build.crm.service.customer.CustomerService;
+import site.easy.to.build.crm.service.lead.LeadServiceImpl;
+import site.easy.to.build.crm.service.ticket.TicketServiceImpl;
 
 @RestController
 public class MyCustomerController {
     
     private final CustomerService customerService;
+    private final TicketServiceImpl ticketServiceImpl;
+    private final LeadServiceImpl leadServiceImpl;
 
     @Autowired
-    public MyCustomerController(CustomerService c){
+    public MyCustomerController(CustomerService c, TicketServiceImpl ticketServiceImpl,LeadServiceImpl leadServiceImpl){
         this.customerService = c;
+        this.ticketServiceImpl = ticketServiceImpl;
+        this.leadServiceImpl = leadServiceImpl;
     }
 
     @GetMapping("/api/all-customers")
@@ -32,5 +40,26 @@ public class MyCustomerController {
         return customers;
     }
 
+    @GetMapping("/api/all-tickets")
+    public List<Ticket> getAllTickets(){
+        List<Ticket> tickets = new ArrayList<>();
+        try {
+            tickets = ticketServiceImpl.findAll();
+        } catch (Exception e){
+            return null;
+        }
+        return tickets;
+    }
+
+    @GetMapping("/api/all-leads")
+    public List<Lead> getAllLeads(){
+        List<Lead> Leads = new ArrayList<>();
+        try {
+            Leads = leadServiceImpl.findAll();
+        } catch (Exception e){
+            return null;
+        }
+        return Leads;
+    }
 
 }
